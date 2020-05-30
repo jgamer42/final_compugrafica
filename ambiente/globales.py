f4a65028515b81afcdb5987b6001013f3162c0e0
Jaime Andres Valencia <jgaimer42@gmail.com>
2020-05-30 02:16:55 -0500

    Desarollo2 (#3)
    * ventana, clase jugador
    
    * test
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Delete jugador.py
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- ambiente/globales.py
+++ ambiente/globales.py
@@ -1,2 +1,7 @@
-#aqui se van a poner todas las variables que necesitan ser 
-#accesibles desde cualquier elemento del programa
+import pygame
+
+# aqui se van a poner todas las variables que necesitan ser
+# accesibles desde cualquier elemento del programa
+
+# SPRITE GROUPS
+jugadores = pygame.sprite.Group()
--- juego.py
+++ juego.py
@@ -1,23 +1,29 @@
-#importante leer NO BORRAR
-#Estandares: 
+# importante leer, son las reglas de desarollo
 # - para este progama se usara snake case (esto_es_un_ejemplo) para la notacion de variables
 # - nombres de variables y archivos en español
 # - el de tema de los parentesis y espacios esta automatizado, cuando hacen el push el los acomoda
 # - a la hora de crear nuevas clase nombrar el archivo en minisculas y la clase con la primera en mayuscula
-# - mantener coherencia entre plural y singular 
+# - mantener coherencia entre plural y singular
 # - tratar de no subir word, psd, fsd, o cualquier extension o archivo compilado
 import pygame
-from jugador.clases import jugador
-from enemigos.clases import enemigo_base
-from bloque.clases import bloque_base
+
 from ambiente.constantes import *
+from ambiente.globales import *
+from bloque.clases import bloque_base
+from enemigos.clases import enemigo_base
+from jugador.clases.jugador import Jugador
+
 en_juego = True
+jugador = Jugador([0, 0])
+jugadores.add(jugador)
 if __name__ == "__main__":
     pygame.init()
-    ventana = pygame.display.set_mode([ANCHO,ALTO])
-    while(en_juego):
+    ventana = pygame.display.set_mode([ANCHO, ALTO])
+    while en_juego:
         for evento in pygame.event.get():
-            if(evento.type == pygame.QUIT):
+            if evento.type == pygame.QUIT:
                 en_juego = False
-    ventana.fill(NEGRO)
-    pygame.display.flip()
+        jugadores.update()
+        ventana.fill(NEGRO)
+        jugadores.draw(ventana)
+        pygame.display.flip()
