9c651ab735b6bba20a56e19c58909cd1fef14e05
Jaime <jgaimer42@gmail.com>
2020-05-30 01:26:33 -0500

    plantilla del proyecto

---
+++ enemigos/clases/enemigo_base.py
@@ -0,0 +1,5 @@
+import sys 
+import os
+sys.path.append(os.getcwd()+"/ambiente/")
+import constantes
+print(constantes.dato + " desde el enemigo")
---
+++ juego.py
@@ -0,0 +1,3 @@
+from jugador.clases import jugador
+from enemigos.clases import enemigo_base
+from bloque.clases import bloque_base
